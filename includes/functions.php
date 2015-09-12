320455410e80c9e9a2b50af7f79f62be4fc6bac3
Andrew Munro <andrew@sumobi.com>
2015-09-12 20:41:36 +1200

    add enable popup function

--- includes/functions.php
+++ includes/functions.php
@@ -8,3 +8,23 @@
 function trustedd_is_edd_active() {
 	return class_exists( 'Easy_Digital_Downloads' );
 }
+
+/**
+ * Posts that should have the lightbox code included
+ */
+function trustedd_enable_popup( $post_id = 0 ) {
+
+	$lightbox = false;
+
+	$posts = apply_filters( 'trustedd_lightbox_posts',
+		array()
+	);
+
+	$changelog = get_post_meta( get_the_ID(), '_edd_sl_changelog', true );
+
+	if ( in_array( $post_id, $posts ) || $changelog ) {
+		$lightbox = true;
+	}
+
+	return apply_filters( 'trustedd_enable_popup', $lightbox );
+}
