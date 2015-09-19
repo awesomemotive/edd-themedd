9214344174318ca0d85417ca9a07c22603622e15
Andrew Munro <andrew@sumobi.com>
2015-09-19 22:34:58 +1200

    add page templates

---
+++ page-templates/slim.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template name: Slim
+ *
+ */
+
+get_header(); ?>
+
+<div class="wrapper slim">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+				// Include the page content template.
+				get_template_part( 'content', 'page' );
+
+			endwhile;
+		?>
+		</main>
+	</div>
+
+</div>
+
+<?php
+get_footer();
---
+++ page-templates/wide.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Wide
+ */
+
+get_header(); ?>
+
+<div class="wrapper wide">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+				// Include the page content template.
+				get_template_part( 'content', 'page' );
+
+			endwhile;
+		?>
+		</main>
+	</div>
+
+</div>
+<?php
+get_footer();
