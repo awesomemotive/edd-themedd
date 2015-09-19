838606fe9db4b9bcf92eeb9b40e402c60f5d1c0d
Andrew Munro <andrew@sumobi.com>
2015-09-19 22:36:18 +1200

    footer tweaks

--- footer.php
+++ footer.php
@@ -10,16 +10,27 @@
 
 <?php do_action( 'trustedd_content_after' ); ?>
 
-	<footer id="footer">
+	<footer id="colophon" class="site-footer" role="contentinfo">
 
 		<?php do_action( 'trustedd_footer_start' ); ?>
 
-		<div class="wrapper aligncenter">
-			<section class="section copyright">
-				<div class="wrapper">
-					<?php printf( __( 'Copyright &copy; %s, %s', 'trustedd' ), date('Y'), get_bloginfo( 'name' ) ); ?>
-				</div>
-			</section>
+		<?php if ( has_nav_menu( 'primary' ) ) : ?>
+			<nav class="main-navigation" role="navigation">
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'primary',
+						'menu_class'     => 'primary-menu',
+					 ) );
+				?>
+			</nav>
+		<?php endif; ?>
+
+		<div class="site-info">
+			<?php
+				do_action( 'trusted_footer' );
+			?>
+
+			<?php printf( __( 'Copyright &copy; %s, %s', 'trustedd' ), date('Y'), get_bloginfo( 'name' ) ); ?>
 		</div>
 
 		<?php do_action( 'trustedd_footer_end' ); ?>
@@ -29,6 +40,5 @@
 </div>
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
