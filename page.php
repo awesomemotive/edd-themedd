e64d1aec015d1d6ba2cde5e25e46870c7704776a
Andrew Munro <andrew@sumobi.com>
2015-09-29 22:37:17 +1300

    allow comments on pages

--- page.php
+++ page.php
@@ -18,6 +18,11 @@ get_header(); ?>
 				// Include the page content template.
 				get_template_part( 'content', 'page' );
 
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+				
 			endwhile;
 		?>
 		</main>
