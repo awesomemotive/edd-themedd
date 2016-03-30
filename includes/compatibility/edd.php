0033c94197ab044bf64e95d4fd5f834efa1565cf
Andrew Munro <andrew@sumobi.com>
2016-03-31 12:37:16 +1300

    CSS

--- includes/compatibility/edd.php
+++ includes/compatibility/edd.php
@@ -1,1 +1,14 @@
 <?php
+
+/**
+ * Enqueue frontend scripts
+ *
+ * @since 3.2
+ */
+function trustedd_edd_remove_css() {
+
+	// remove software licensing CSS file
+	wp_dequeue_style( 'edd-sl-styles' );
+
+}
+add_action( 'wp_enqueue_scripts', 'trustedd_edd_remove_css' );
