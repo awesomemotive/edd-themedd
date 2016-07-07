d41f369bd70d25041476b3edf87ac1745c2364de
Andrew Munro <andrew@sumobi.com>
2016-07-07 16:38:22 +1200

    template tag modifications

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -3,8 +3,6 @@
  * Template tags
  */
 
-
-if ( ! function_exists( 'themedd_comment_nav' ) ) :
 /**
 * Display navigation to next/previous comments when applicable.
 *
@@ -31,55 +29,50 @@ function themedd_comment_nav() {
 	<?php
 	endif;
 }
-endif;
+
+/**
+ * Prints HTML with meta information for the categories, tags.
+ *
+ * Create your own themedd_entry_meta() to override in a child theme.
+ *
+ * @since 1.0
+ */
+function themedd_entry_meta() {
+	if ( 'post' == get_post_type() ) {
+		$author_avatar_size = apply_filters( 'themedd_author_avatar_size', 49 );
+		printf( '<span class="byline"><span class="author vcard">%1$s<span class="screen-reader-text">%2$s </span> <a class="url fn n" href="%3$s">%4$s</a></span></span>',
+			get_avatar( get_the_author_meta( 'user_email' ), $author_avatar_size ),
+			esc_html_x( 'Author', 'Used before post author name.', 'themedd' ),
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			esc_html( get_the_author() )
+		);
+	}
+
+	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
+		themedd_entry_date();
+	}
+
+	$format = get_post_format();
+	if ( current_theme_supports( 'post-formats', $format ) ) {
+		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
+			sprintf( '<span class="screen-reader-text">%s </span>', esc_html_x( 'Format', 'Used before post format.', 'themedd' ) ),
+			esc_url( get_post_format_link( $format ) ),
+			esc_html( get_post_format_string( $format ) )
+		);
+	}
+
+	if ( 'post' == get_post_type() ) {
+		themedd_entry_taxonomies();
+	}
+
+	if ( ! is_singular() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'themedd' ), get_the_title() ) );
+		echo '</span>';
+	}
+}
 
 
- if ( ! function_exists( 'themedd_entry_meta' ) ) :
- /**
-  * Prints HTML with meta information for the categories, tags.
-  *
-  * Create your own themedd_entry_meta() to override in a child theme.
-  *
-  * @since 1.0
-  */
- function themedd_entry_meta() {
- 	if ( 'post' == get_post_type() ) {
- 		$author_avatar_size = apply_filters( 'themedd_author_avatar_size', 49 );
- 		printf( '<span class="byline"><span class="author vcard">%1$s<span class="screen-reader-text">%2$s </span> <a class="url fn n" href="%3$s">%4$s</a></span></span>',
- 			get_avatar( get_the_author_meta( 'user_email' ), $author_avatar_size ),
- 			esc_html_x( 'Author', 'Used before post author name.', 'themedd' ),
- 			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
- 			esc_html( get_the_author() )
- 		);
- 	}
-
- 	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
- 		themedd_entry_date();
- 	}
-
- 	$format = get_post_format();
- 	if ( current_theme_supports( 'post-formats', $format ) ) {
- 		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
- 			sprintf( '<span class="screen-reader-text">%s </span>', esc_html_x( 'Format', 'Used before post format.', 'themedd' ) ),
- 			esc_url( get_post_format_link( $format ) ),
- 			esc_html( get_post_format_string( $format ) )
- 		);
- 	}
-
- 	if ( 'post' == get_post_type() ) {
- 		themedd_entry_taxonomies();
- 	}
-
- 	if ( ! is_singular() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
- 		echo '<span class="comments-link">';
- 		comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'themedd' ), get_the_title() ) );
- 		echo '</span>';
- 	}
- }
- endif;
-
-
-if ( ! function_exists( 'themedd_entry_date' ) ) :
 /**
 * Print HTML with date information for current post.
 *
@@ -107,9 +100,9 @@ function themedd_entry_date() {
 		$time_string
 	);
 }
-endif;
 
-if ( ! function_exists( 'themedd_entry_taxonomies' ) ) :
+
+
 /**
  * Print HTML with category and tags for current post.
  *
@@ -137,7 +130,7 @@ function themedd_entry_taxonomies() {
 	}
 
 }
-endif;
+
 
 
 
@@ -161,8 +154,6 @@ function themedd_excerpt( $class = 'entry-summary' ) {
 }
 
 
-
-if ( ! function_exists( 'themedd_excerpt_more' ) && ! is_admin() ) :
 /**
  * Replaces "[...]" (appended to automatically generated excerpts) with ... and a 'Continue reading' link.
  *
@@ -178,7 +169,7 @@ function themedd_excerpt_more() {
 	return ' &hellip; ' . $link;
 }
 add_filter( 'excerpt_more', 'themedd_excerpt_more' );
-endif;
+
 
 /**
  * Determine whether blog/site has more than one category.
@@ -214,7 +205,6 @@ function themedd_categorized_blog() {
 
 }
 
-if ( ! function_exists( 'themedd_post_thumbnail' ) ) :
 /**
  * Display an optional post thumbnail.
  *
@@ -251,4 +241,3 @@ function themedd_post_thumbnail() {
 
 	<?php endif; // End is_singular()
 }
-endif;
