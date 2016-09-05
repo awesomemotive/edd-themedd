f5be9e1de0072237f43c8e3e2bc1ae8a81e48fbc
Andrew Munro <andrew@sumobi.com>
2016-09-05 22:17:37 +1200

    Remove post meta from sidebar

--- includes/custom.php
+++ includes/custom.php
@@ -1,33 +1,41 @@
 <?php
 
 /**
- * Post/entry meta shown in sidebar
+ * Load the featured image on the themedd_article_start hook
+ * This allows us to remove the featured image dynamically where needed
  *
  * @since 1.0.0
  */
-function themedd_post_meta() {
-
-    if ( ! is_singular( 'post' ) ) {
-        return;
-    }
-
-    ?>
-
-    <footer class="entry-footer">
-		<?php themedd_entry_meta(); ?>
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit %s', 'themedd' ),
-					the_title( '<span class="screen-reader-text">', '</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer>
-
-    <?php
+function themedd_load_post_thumbnail() {
+	themedd_post_thumbnail();
 }
-add_action( 'themedd_primary_sidebar_start', 'themedd_post_meta' );
+add_action( 'themedd_article_start', 'themedd_load_post_thumbnail' );
+
+/**
+ * Load the post header on the themedd_single_start hook
+ * This allows us to remove the header dynamically where needed
+ *
+ * @since 1.0.0
+ */
+function themedd_load_post_header() {
+	themedd_post_header();
+}
+add_action( 'themedd_single_start', 'themedd_load_post_header' );
+
+/**
+ * Load the biography template after the entry content
+ *
+ * @since 1.0.0
+ */
+function themedd_load_biography() {
+
+	if ( ! is_singular( 'post' ) ) {
+		return;
+	}
+
+	if ( '' !== get_the_author_meta( 'description' ) ) {
+		get_template_part( 'template-parts/biography' );
+	}
+
+}
+add_action( 'themedd_entry_content_end', 'themedd_load_biography' );
