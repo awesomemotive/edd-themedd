c104b5a4314af9b1c2486b0217b89eb37eccf34b
Andrew Munro <andrew@sumobi.com>
2016-10-20 15:46:48 +1300

    improve CSS structure

---
+++ js/src/navigation.js
@@ -0,0 +1,77 @@
+/**
+ * navigation.js
+ *
+ * Handles toggling the navigation menu for small screens.
+ * Also adds a focus class to parent li's for accessibility.
+ * Finally adds a class required to reveal the search in the handheld footer bar.
+ */
+( function() {
+	// Add class to footer search when clicked
+	// jQuery( window ).load( function() {
+	// 	jQuery( '.storefront-handheld-footer-bar .search > a' ).click( function(e) {
+	// 		jQuery( this ).parent().toggleClass( 'active' );
+	// 		e.preventDefault();
+	// 	});
+	// });
+
+	var container, button, menu;
+
+	container = document.getElementById( 'site-navigation' );
+
+
+
+	if ( ! container ) {
+		return;
+	}
+
+	//console.log( container );
+
+	button = container.getElementsByTagName( 'button' )[0];
+
+	if ( 'undefined' === typeof button ) {
+		return;
+	}
+
+//	console.log( button );
+
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	console.log( menu );
+
+	// Hide menu toggle button if menu is empty and return early.
+	if ( 'undefined' === typeof menu ) {
+		button.style.display = 'none';
+		return;
+	}
+
+	menu.setAttribute( 'aria-expanded', 'false' );
+
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+		menu.className += ' nav-menu';
+	}
+
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+			container.className = container.className.replace( ' toggled', '' );
+			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+		} else {
+			container.className += ' toggled';
+			button.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
+		}
+	};
+
+	// Add focus class to li
+	jQuery( '.main-navigation, .secondary-navigation' ).find( 'a' ).on( 'focus.storefront blur.storefront', function() {
+		jQuery( this ).parents().toggleClass( 'focus' );
+	});
+
+	// Add focus to cart dropdown
+	// jQuery( window ).load( function() {
+	// 	jQuery( '.site-header-cart' ).find( 'a' ).on( 'focus.storefront blur.storefront', function() {
+	// 		jQuery( this ).parents().toggleClass( 'focus' );
+	// 	});
+	// });
+
+} )();
