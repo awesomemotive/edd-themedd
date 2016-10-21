8c1d78858f58c58c90f3990d0f8dee68c67630f5
Andrew Munro <andrew@sumobi.com>
2016-10-22 11:43:14 +1300

    add navigation.js

--- js/src/navigation.js
+++ js/src/navigation.js
@@ -1,77 +1,117 @@
+/* global screenReaderText */
 /**
- * navigation.js
+ * Theme functions file.
  *
- * Handles toggling the navigation menu for small screens.
- * Also adds a focus class to parent li's for accessibility.
- * Finally adds a class required to reveal the search in the handheld footer bar.
+ * Contains handlers for navigation and widget area.
  */
-( function() {
-	// Add class to footer search when clicked
-	// jQuery( window ).load( function() {
-	// 	jQuery( '.storefront-handheld-footer-bar .search > a' ).click( function(e) {
-	// 		jQuery( this ).parent().toggleClass( 'active' );
-	// 		e.preventDefault();
-	// 	});
-	// });
 
-	var container, button, menu;
+( function( $ ) {
+	var $body, masthead, menuToggle, siteNavigation, socialNavigation, siteHeaderMenu, resizeTimer;
 
-	container = document.getElementById( 'site-navigation' );
+	function initMainNavigation( container ) {
 
+		// Add dropdown toggle that display child menu items.
+		container.find( '.menu-item-has-children > a' ).after( '<button class="dropdown-toggle" aria-expanded="false">' + screenReaderText.expand + '</button>' );
 
+		// Toggle buttons and submenu items with active children menu items.
+		container.find( '.current-menu-ancestor > button' ).addClass( 'toggled-on' );
+		container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
 
-	if ( ! container ) {
-		return;
+		// Add menu items with submenus to aria-haspopup="true".
+		container.find( '.menu-item-has-children' ).attr( 'aria-haspopup', 'true' );
+
+		container.find( '.dropdown-toggle' ).click( function( e ) {
+			var _this = $( this );
+			e.preventDefault();
+			_this.toggleClass( 'toggled-on' );
+			_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
+
+			// jscs:disable
+			_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+			// jscs:enable
+			_this.html( _this.html() === screenReaderText.expand ? screenReaderText.collapse : screenReaderText.expand );
+		} );
 	}
+	initMainNavigation( $( '.main-navigation' ) );
 
-	//console.log( container );
+	masthead         = $( '#masthead' );
+	menuToggle       = $( '#menu-toggle' );
+	siteHeaderMenu   = $( '#site-header-menu' );
+	siteNavigation   = $( '#site-navigation' );
 
-	button = container.getElementsByTagName( 'button' )[0];
+	// Enable menuToggle.
+	( function() {
 
-	if ( 'undefined' === typeof button ) {
-		return;
-	}
-
-//	console.log( button );
-
-	menu = container.getElementsByTagName( 'ul' )[0];
-
-	console.log( menu );
-
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
-
-	menu.setAttribute( 'aria-expanded', 'false' );
-
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
-
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
-			button.setAttribute( 'aria-expanded', 'false' );
-			menu.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			container.className += ' toggled';
-			button.setAttribute( 'aria-expanded', 'true' );
-			menu.setAttribute( 'aria-expanded', 'true' );
+		// Return early if menuToggle is missing.
+		if ( ! menuToggle ) {
+			return;
 		}
-	};
 
-	// Add focus class to li
-	jQuery( '.main-navigation, .secondary-navigation' ).find( 'a' ).on( 'focus.storefront blur.storefront', function() {
-		jQuery( this ).parents().toggleClass( 'focus' );
-	});
+		// Add an initial values for the attribute.
+		menuToggle.add( siteNavigation ).attr( 'aria-expanded', 'false' );
 
-	// Add focus to cart dropdown
-	// jQuery( window ).load( function() {
-	// 	jQuery( '.site-header-cart' ).find( 'a' ).on( 'focus.storefront blur.storefront', function() {
-	// 		jQuery( this ).parents().toggleClass( 'focus' );
-	// 	});
-	// });
+		menuToggle.on( 'click.themedd', function() {
+			$( this ).add( siteHeaderMenu ).toggleClass( 'toggled-on' );
 
-} )();
+			// jscs:disable
+			$( this ).add( siteNavigation ).attr( 'aria-expanded', $( this ).add( siteNavigation ).attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+			// jscs:enable
+		} );
+	} )();
+
+	// Fix sub-menus for touch devices and better focus for hidden submenu items for accessibility.
+	( function() {
+		if ( ! siteNavigation || ! siteNavigation.children().length ) {
+			return;
+		}
+
+		if ( 'ontouchstart' in window ) {
+			siteNavigation.find( '.menu-item-has-children > a' ).on( 'touchstart.themedd', function( e ) {
+				var el = $( this ).parent( 'li' );
+
+				if ( ! el.hasClass( 'focus' ) ) {
+					e.preventDefault();
+					el.toggleClass( 'focus' );
+					el.siblings( '.focus' ).removeClass( 'focus' );
+				}
+			} );
+		}
+
+		siteNavigation.find( 'a' ).on( 'focus.themedd blur.themedd', function() {
+			$( this ).parents( '.menu-item' ).toggleClass( 'focus' );
+		} );
+	} )();
+
+	// Add he default ARIA attributes for the menu toggle and the navigations.
+	function onResizeARIA() {
+		if ( 910 > window.innerWidth ) {
+			if ( menuToggle.hasClass( 'toggled-on' ) ) {
+				menuToggle.attr( 'aria-expanded', 'true' );
+			} else {
+				menuToggle.attr( 'aria-expanded', 'false' );
+			}
+
+			if ( siteHeaderMenu.hasClass( 'toggled-on' ) ) {
+				siteNavigation.attr( 'aria-expanded', 'true' );
+			} else {
+				siteNavigation.attr( 'aria-expanded', 'false' );
+			}
+
+			menuToggle.attr( 'aria-controls', 'site-navigation social-navigation' );
+		} else {
+			menuToggle.removeAttr( 'aria-expanded' );
+			siteNavigation.removeAttr( 'aria-expanded' );
+			menuToggle.removeAttr( 'aria-controls' );
+		}
+	}
+
+	$( document ).ready( function() {
+		$body = $( document.body );
+
+		$( window )
+			.on( 'load.themedd', onResizeARIA )
+			.on( 'resize.themedd', function() {
+				onResizeARIA();
+			} );
+	} );
+} )( jQuery );
