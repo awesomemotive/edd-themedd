e2c32d615601d3b8f74c2ac119810fde239710c0
Andrew Munro <andrew@sumobi.com>
2016-10-28 22:35:05 +1300

    Remove links from prices

--- edd_templates/shortcode-content-price.php
+++ edd_templates/shortcode-content-price.php
@@ -1,28 +1,26 @@
+<?php $item_props = edd_add_schema_microdata() ? ' itemprop="offers" itemscope itemtype="http://schema.org/Offer"' : ''; ?>
+
 <?php if ( '0' == edd_get_download_price( get_the_ID() ) ) :
 // Free download
 ?>
-	<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-		<div itemprop="price" class="edd_price">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">Free</a>
-		</div>
+	<div<?php echo $item_props; ?>>
+		<div itemprop="price" class="edd_price">Free</div>
 	</div>
 
 <?php elseif ( edd_has_variable_prices( get_the_ID() ) ) :
 // Variable priced download
 ?>
 
-	<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-		<div itemprop="price" class="edd_price">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">From <?php edd_price( get_the_ID() ); ?></a>
+	<div<?php echo $item_props; ?>>
+		<div itemprop="price" class="edd_price">From <?php edd_price( get_the_ID() ); ?></a>
 		</div>
 	</div>
 
 <?php elseif ( ! edd_has_variable_prices( get_the_ID() ) ) :
 // normal priced download
 ?>
-	<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-		<div itemprop="price" class="edd_price">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php edd_price( get_the_ID() ); ?></a>
+	<div<?php echo $item_props; ?>>
+		<div itemprop="price" class="edd_price"><?php edd_price( get_the_ID() ); ?>
 		</div>
 	</div>
 <?php endif; ?>
