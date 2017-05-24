e99cd3cdcfc95ae00baedc6aaa3e25ba529f50a1
Andrew Munro <andrew@sumobi.com>
2017-05-24 19:50:00 +1200

    Add download-grid.php template file
    Keeps all existing EDD hooks and template part calls

---
+++ template-parts/download-grid.php
@@ -0,0 +1,33 @@
+<?php $schema = edd_add_schema_microdata() ? 'itemscope itemtype="http://schema.org/Product" ' : ''; ?>
+
+<div <?php echo $schema; ?>class="edd_download" id="edd_download_<?php echo get_the_ID(); ?>">
+	<div class="edd_download_inner">
+
+		<?php
+
+		do_action( 'edd_download_before' );
+
+		edd_get_template_part( 'shortcode', 'content-image' );
+		
+		do_action( 'edd_download_after_thumbnail' );
+
+		edd_get_template_part( 'shortcode', 'content-title' );
+
+		do_action( 'edd_download_after_title' );
+
+		edd_get_template_part( 'shortcode', 'content-excerpt' );
+
+		do_action( 'edd_download_after_content' );
+
+		edd_get_template_part( 'shortcode', 'content-price' );
+
+		do_action( 'edd_download_after_price' );
+
+		edd_get_template_part( 'shortcode', 'content-cart-button' );
+
+		do_action( 'edd_download_after' );
+
+		?>
+
+	</div>
+</div>
