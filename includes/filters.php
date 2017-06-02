9faeffe1abb7754b9b2f4114f0164f5edebe54ee
Andrew Munro <andrew@sumobi.com>
2017-06-02 19:24:12 +1200

    Add comment
    + move themedd_excerpt_more() function

--- includes/filters.php
+++ includes/filters.php
@@ -19,6 +19,7 @@ function themedd_body_classes( $classes ) {
 		$classes[] = 'no-sidebar';
 	}
 
+	// Add "slim" body class when using the page template
 	if ( is_page_template( 'page-templates/slim.php' ) ) {
 		$classes[] = 'slim';
 	}
@@ -27,3 +28,26 @@ function themedd_body_classes( $classes ) {
 
 }
 add_filter( 'body_class', 'themedd_body_classes' );
+
+/**
+ * Replaces "[...]" (appended to automatically generated excerpts) with ... and a 'Continue reading' link.
+ *
+ * @since 1.0.0
+ *
+ * @return string 'Continue reading' link prepended with an ellipsis.
+ */
+if ( ! function_exists( 'themedd_excerpt_more' ) ) :
+function themedd_excerpt_more( $link ) {
+
+	if ( is_admin() ) {
+		return $link;
+	}
+
+	$link = sprintf( '<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
+		esc_url( get_permalink( get_the_ID() ) ),
+		sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'themedd' ), get_the_title( get_the_ID() ) )
+	);
+	return ' &hellip; ' . $link;
+}
+add_filter( 'excerpt_more', 'themedd_excerpt_more' );
+endif;
