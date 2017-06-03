786bce4186fff8dd0a2579b9115f439f8af913ff
Andrew Munro <andrew@sumobi.com>
2017-06-03 19:31:52 +1200

    Load entry footer via hook

--- includes/actions.php
+++ includes/actions.php
@@ -94,18 +94,54 @@ endif;
 add_action( 'after_setup_theme', 'themedd_setup' );
 
 /**
- * Load various elements on the themedd_page_header_wrapper_start action hook
+ * Show the date and author in the header on single posts
  *
  * @since 1.0.0
- *
- * @return string
  */
-function themedd_page_header_wrapper_start() {
+function themedd_show_posted_on() {
 
-	// Add the date to the header on single posts
 	if ( is_singular( 'post' ) ) {
 		themedd_posted_on();
 	}
 
 }
-add_action( 'themedd_page_header_wrapper_start', 'themedd_page_header_wrapper_start' );
+add_action( 'themedd_page_header_wrapper_start', 'themedd_show_posted_on' );
+
+/**
+ * Show the featured image on the themedd_article_start hook.
+ * This allows us to remove the featured image dynamically where needed.
+ *
+ * @since 1.0.0
+ */
+function themedd_show_post_thumbnail() {
+	themedd_post_thumbnail();
+}
+add_action( 'themedd_article_start', 'themedd_show_post_thumbnail' );
+
+/**
+ * Show the entry footer info (Categories and tags + edit link).
+ *
+ * @since 1.0.0
+ */
+function themedd_show_entry_footer() {
+
+	if ( is_singular( 'post' ) ) {
+		themedd_entry_footer();
+	}
+
+}
+add_action( 'themedd_entry_content_end', 'themedd_show_entry_footer' );
+
+/**
+ * Load the biography template after the entry content on a single post.
+ *
+ * @since 1.0.0
+ */
+function themedd_show_author_biography() {
+
+	if ( is_singular( 'post' ) && '' !== get_the_author_meta( 'description' ) ) {
+		get_template_part( 'template-parts/biography' );
+	}
+
+}
+add_action( 'themedd_entry_content_end', 'themedd_show_author_biography' );
