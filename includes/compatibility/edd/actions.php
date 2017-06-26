1517d0632a0f4fc530521703dd296094c9a9ef1e
Andrew Munro <andrew@sumobi.com>
2017-06-26 19:26:08 +1200

    Remove themedd_edd_download_info()

--- includes/compatibility/edd/actions.php
+++ includes/compatibility/edd/actions.php
@@ -26,38 +26,6 @@ remove_action( 'wp_enqueue_scripts', 'edd_register_styles' );
 remove_action( 'edd_after_download_content', 'edd_append_purchase_link' );
 
 /**
- * The combined price and purchase button shown on the single download page
- *
- * @since 1.0.0
- */
-function themedd_edd_download_info() {
-
-    if ( ! is_singular( 'download' ) ) {
-        return;
-    }
-
-    ?>
-	<section class="download-info">
-		<?php
-
-			do_action( 'themedd_edd_download_info_start', get_the_ID() );
-
-			// Display download price.
-			echo themedd_edd_price();
-
-			do_action( 'themedd_edd_download_info_after_price', get_the_ID() );
-
-			// Display purchase link.
-            themedd_edd_purchase_link();
-
-			do_action( 'themedd_edd_download_info_end', get_the_ID() );
-        ?>
-	</section>
-<?php
-}
-add_action( 'themedd_sidebar_download', 'themedd_edd_download_info' );
-
-/**
  * Add cart link to secondary menu
  *
  * @since 1.0.0
