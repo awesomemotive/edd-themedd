0ecd00b01d4281482be8db40666a073e417b1f54
Andrew Munro <andrew@sumobi.com>
2017-06-26 19:27:11 +1200

    Add author_url() method to FES class

--- includes/compatibility/edd/class-frontend-submissions.php
+++ includes/compatibility/edd/class-frontend-submissions.php
@@ -53,5 +53,23 @@ class Themedd_EDD_Frontend_Submissions {
 		return $classes;
 	}
 
+	/**
+	 * Get the FES vendor URL
+	 */
+	public function author_url( $author = null ) {
+
+		if ( ! $author ) {
+			$author = wp_get_current_user();
+		} else {
+			$author = new WP_User( $author );
+		}
+
+		if ( ! class_exists( 'EDD_Front_End_Submissions' ) ) {
+			return get_author_posts_url( $author->ID, $author->user_nicename );
+		}
+
+		return EDD_FES()->vendors->get_vendor_store_url( $author->ID );
+	}
+
 }
 new Themedd_EDD_Frontend_Submissions;
