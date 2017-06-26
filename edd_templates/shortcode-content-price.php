e9e30f50222be9844e86f053ff86a26cc8372842
Andrew Munro <andrew@sumobi.com>
2017-06-26 19:21:00 +1200

    Formatting

--- edd_templates/shortcode-content-price.php
+++ edd_templates/shortcode-content-price.php
@@ -1,23 +1,26 @@
 <?php $item_props = edd_add_schema_microdata() ? ' itemprop="offers" itemscope itemtype="http://schema.org/Offer"' : ''; ?>
 
-<?php if ( edd_is_free_download( get_the_ID() ) ) :
-// Free download
-?>
+<?php
+/**
+ * Free download.
+ */
+if ( edd_is_free_download( get_the_ID() ) ) : ?>
 	<div<?php echo $item_props; ?>>
 		<div itemprop="price"><span class="edd_price" id="edd_price_<?php echo get_the_id(); ?>"><?php _e( 'Free', 'themedd' ); ?></span></div>
 	</div>
-
-<?php elseif ( edd_has_variable_prices( get_the_ID() ) ) :
-// Variable priced download
-?>
-
+<?php
+/**
+ * Variable priced download
+ */
+elseif ( edd_has_variable_prices( get_the_ID() ) ) : ?>
 	<div<?php echo $item_props; ?>>
 		<div itemprop="price">From <?php edd_price( get_the_ID() ); ?></a></div>
 	</div>
-
-<?php elseif ( ! edd_has_variable_prices( get_the_ID() ) ) :
-// normal priced download
-?>
+<?php
+/**
+ * Normal priced download.
+ */
+elseif ( ! edd_has_variable_prices( get_the_ID() ) ) : ?>
 	<div<?php echo $item_props; ?>>
 		<div itemprop="price"><?php edd_price( get_the_ID() ); ?></div>
 	</div>
