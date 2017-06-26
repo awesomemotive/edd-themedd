13eb5513ce6e1e1af5ac412cb4092e40e2dd3721
Andrew Munro <andrew@sumobi.com>
2017-06-26 19:16:18 +1200

    Add themedd_entry_article_start hook to content-download.php

--- template-parts/content-download.php
+++ template-parts/content-download.php
@@ -7,6 +7,8 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+	<?php do_action( 'themedd_entry_article_start' ); ?>
+
     <?php themedd_post_thumbnail(); ?>
 
     <div class="entry-content">
