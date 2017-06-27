9aa9ff1e38a3bf4e6bc78020a477a187f353613a
Andrew Munro <andrew@sumobi.com>
2017-06-27 17:28:48 +1200

    Add ! function_exists() to themedd_edd_download_nav()

--- includes/compatibility/edd/functions.php
+++ includes/compatibility/edd/functions.php
@@ -102,32 +102,32 @@ function themedd_edd_download_columns() {
  *
  * @since 1.0.0
  */
-function themedd_edd_download_nav() {
+if ( ! function_exists( 'themedd_edd_download_nav' ) ) :
+	function themedd_edd_download_nav() {
 
-	global $wp_query;
+		global $wp_query;
 
-	$big          = 999999;
-	$search_for   = array( $big, '#038;' );
-	$replace_with = array( '%#%', '&' );
+		$big          = 999999;
+		$search_for   = array( $big, '#038;' );
+		$replace_with = array( '%#%', '&' );
 
-	$pagination = paginate_links( array(
-		'base'      => str_replace( $search_for, $replace_with, get_pagenum_link( $big ) ),
-		'format'    => '?paged=%#%',
-		'current'   => max( 1, get_query_var( 'paged' ) ),
-		'total'     => $wp_query->max_num_pages,
-		'prev_text' => __( 'Previous', 'themedd' ),
-		'next_text' => __( 'Next', 'themedd' ),
-	) );
-	?>
+		$pagination = paginate_links( array(
+			'base'    => str_replace( $search_for, $replace_with, get_pagenum_link( $big ) ),
+			'format'  => '?paged=%#%',
+			'current' => max( 1, get_query_var( 'paged' ) ),
+			'total'   => $wp_query->max_num_pages
+		) );
+		?>
 
-	<?php if ( ! empty( $pagination ) ) : ?>
-	<div id="edd_download_pagination" class="navigation">
-		<?php echo $pagination; ?>
-	</div>
-	<?php endif; ?>
+		<?php if ( ! empty( $pagination ) ) : ?>
+		<div id="edd_download_pagination" class="navigation">
+			<?php echo $pagination; ?>
+		</div>
+		<?php endif; ?>
 
-<?php
-}
+	<?php
+	}
+endif;
 
 /**
  * Distraction Free Checkout
