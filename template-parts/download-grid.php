2781c5bc0f15b3205b9d74b8fc95fbcbfb7db5e6
Andrew Munro <andrew@sumobi.com>
2017-07-01 10:20:11 +1200

    Don’t show price if price inside download meta is enabled #71

--- template-parts/download-grid.php
+++ template-parts/download-grid.php
@@ -1,4 +1,7 @@
-<?php $schema = edd_add_schema_microdata() ? 'itemscope itemtype="http://schema.org/Product" ' : ''; ?>
+<?php
+$schema        = edd_add_schema_microdata() ? 'itemscope itemtype="http://schema.org/Product" ' : '';
+$download_meta = themedd_edd_download_meta_options();
+?>
 
 <div <?php echo $schema; ?>class="edd_download mb-xs-4 mb-sm-0" id="edd_download_<?php echo get_the_ID(); ?>">
 	<div class="edd_download_inner">
@@ -12,7 +15,7 @@
 		do_action( 'edd_download_after_thumbnail' );
 
 		do_action( 'edd_download_before_title' );
-		
+
 		edd_get_template_part( 'shortcode', 'content-title' );
 
 		do_action( 'edd_download_after_title' );
@@ -21,7 +24,10 @@
 
 		do_action( 'edd_download_after_content' );
 
-		edd_get_template_part( 'shortcode', 'content-price' );
+		// Only show the standard price if the download meta does not include the price.
+		if ( false === $download_meta['price'] ) {
+			edd_get_template_part( 'shortcode', 'content-price' );
+		}
 
 		do_action( 'edd_download_after_price' );
 
