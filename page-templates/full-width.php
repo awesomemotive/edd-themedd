99c8c83230beb81e4c9fecf3e56ae4e9877db18d
David Sherlock <info@creativenevada.com>
2017-09-04 23:34:19 +0100

    Add files via upload

---
+++ page-templates/full-width.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Full Width
+ */
+
+get_header();
+themedd_page_header();
+?>
+
+<div class="content-wrapper<?php echo themedd_wrapper_classes(); ?>">
+	<div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		do_action( 'themedd_main_start' );
+
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content', 'page' );
+
+      // If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+		endwhile;
+
+		do_action( 'themedd_main_end' );
+		?>
+
+		</main>
+	</div>
+</div>
+
+<?php
+get_footer();
