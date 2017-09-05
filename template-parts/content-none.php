56aec6b71904d84d2f307eeeeaaf69c752b59bd5
Andrew Munro <andrew@amdrew.com>
2017-09-06 09:19:13 +1200

    Improve search results #126
    #126

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -4,10 +4,6 @@
  */
 ?>
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'themedd' ); ?></h1>
-	</header>
-
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
