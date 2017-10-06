e397e8eb2cff0c90019233e8cb02344f4be9a8ff
Andrew Munro <andrew@amdrew.com>
2017-10-07 00:14:28 +1300

    Load download-grid template file for EDD search results #30

--- search.php
+++ search.php
@@ -18,24 +18,47 @@ themedd_page_header( array( 'title' => $page_title, 'subtitle' => sprintf( __( '
 
 	<div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
 		<main id="main" class="site-main" role="main">
-
+			
 			<?php if ( have_posts() ) : ?>
 
-				<?php
-				// Start the loop.
-				while ( have_posts() ) : the_post();
+				<?php if ( themedd_is_edd_active() && Themedd_Search::is_product_search_results() ) : ?>
+					
+				<div class="<?php echo themedd_edd_downloads_list_wrapper_classes(); ?>">
+					
+					<?php while ( have_posts() ) : the_post(); ?>
 
+						<?php get_template_part( 'template-parts/download-grid' ); ?>
+
+					<?php endwhile; ?>
+				
+					<?php
 					/**
-					 * Run the loop for the search to output the results.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-search.php and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', 'search' );
+					* Download pagination
+					*/
+					themedd_edd_download_nav();
+					?>
+							
+				</div>
 
-				// End the loop.
-				endwhile;
+				<?php else : ?>
+				
+					<?php while ( have_posts() ) : the_post();
 
-				themedd_paging_nav();
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'search' );
+
+					// End the loop.
+					endwhile; ?>
+				
+				<?php themedd_paging_nav(); ?>
+
+				<?php endif; ?>
+
+			<?php
 
 			// If no content, include the "No posts found" template.
 			else :
