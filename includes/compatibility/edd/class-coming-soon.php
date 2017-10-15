9faa8842cf206633fd8032f7f38f421eb5042391
Andrew Munro <andrew@amdrew.com>
2017-10-15 22:30:17 +1300

    Refactor coming soon class

--- includes/compatibility/edd/class-coming-soon.php
+++ includes/compatibility/edd/class-coming-soon.php
@@ -27,19 +27,65 @@ class Themedd_EDD_Coming_Soon {
 		// Remove the icon class.
 		add_filter( 'edd_cs_btn_icon', array( $this, 'remove_icon' ) );
 
-		// Remove the price when the download is coming soon.
+		// Remove the price on a single download page when the download is coming soon.
 		add_action( 'template_redirect', array( $this, 'remove_price' ) );
 
-		// Hide various elements in the download grid for "Coming Soon" downloads.
-		add_filter( 'themedd_edd_show_price', array( $this, 'hide_download_grid_price' ), 10, 1 );
-		add_filter( 'themedd_edd_show_buy_button', array( $this, 'hide_download_grid_buy_button' ), 10, 1 );
-		
 		// Remove the "Coming Soon" text at the end of a download in the download grid.
 		remove_action( 'edd_download_after', array( edd_coming_soon(), 'display_text' ) );
 		
 		// Add the "Coming Soon text back, but at the start of the download footer, so it's above the voting form.
-		add_action( 'themedd_edd_download_footer_start', array( edd_coming_soon(), 'display_text' ), 1 );
+		add_action( 'themedd_edd_download_footer_start', array( edd_coming_soon(), 'display_text' ), 10, 1 );
 
+		// Allow the download footer to show.
+		add_filter( 'themedd_edd_download_footer', array( $this, 'show_download_footer' ), 10, 2 );
+
+		// Filter the download grid options.
+		add_filter( 'themedd_edd_download_grid_options', array( $this, 'download_grid_options' ), 10, 1 );
+		
+	}
+
+	/**
+	 * Filters the download grid options.
+	 *
+	 * @access  public
+	 * @since   1.0.3
+	 */
+	public function download_grid_options( $options ) {
+
+		// Get the download ID.
+		$download_id = get_the_ID();
+
+		/**
+		 * Disable the price for "coming soon" downloads.
+		 */
+		if ( edd_coming_soon_is_active( $download_id ) ) {
+			$options['price'] = false;
+		}
+
+		/**
+		 * Prevent the shortcode-content-cart-button.php template file from loading if the download is "Coming Soon" and voting isn't enabled.
+		 * This essentially removes the empty HTML markup: <div class="edd_download_buy_button"></div>
+		 */
+		if ( edd_coming_soon_is_active( $download_id ) && ! edd_coming_soon_voting_enabled( $download_id ) ) {
+			$options['buy_button'] = false;
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Show the download footer so the coming soon text can show.
+	 *
+	 * @access  public
+	 * @since   1.0.3
+	 */
+	public function show_download_footer( $show, $download_id ) {
+
+		if ( edd_coming_soon_is_active( $download_id ) ) {
+			$show = true;
+		}
+
+		return $show;
 	}
 
 	/**
@@ -49,7 +95,7 @@ class Themedd_EDD_Coming_Soon {
 	 * @since   1.0.2
 	 */
 	public function remove_price() {
-		
+
 		// No need to go any further if price enhancements has been disabled.
 		if ( false === themedd_edd_price_enhancements() ) {
 			return;
@@ -96,41 +142,6 @@ class Themedd_EDD_Coming_Soon {
 	 */
 	public function remove_icon( $icon ) {
 		return '';
-	}
-
-	/**
-	 * Hide the price of "coming soon" downloads in the download grid.
-	 *
-	 * @access  public
-	 * @since   1.0.2
-	 */
-	public function hide_download_grid_price( $return ) {
-		
-		if ( edd_coming_soon_is_active( get_the_ID() ) ) {
-			$return = false;
-		}
-
-		return $return;
-
-	}
-
-	/**
-	 * Prevent the shortcode-content-cart-button.php template file from loading if the download is "Coming Soon" and voting isn't enabled.
-	 * This essentially hides an empty div in the download footer.
-	 *
-	 * @access  public
-	 * @since   1.0.2
-	 */
-	public function hide_download_grid_buy_button( $return ) {
-
-		$download_id = get_the_ID();
-
-		if ( edd_coming_soon_is_active( $download_id ) && ! edd_coming_soon_voting_enabled( $download_id ) ) {
-			$return = false;
-		}
-
-		return $return;
-
 	}
 
 }
