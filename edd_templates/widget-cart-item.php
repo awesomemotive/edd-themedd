a999af6c99f7f5def08166817d7cfaa9bc3cfc20
fxbenard <fx@fxbenard.com>
2018-01-08 16:37:05 +0100

    Wrong textdomain

--- edd_templates/widget-cart-item.php
+++ edd_templates/widget-cart-item.php
@@ -3,5 +3,5 @@
 	<span class="edd-cart-item-quantity">{item_quantity} @</span>
 	<span class="edd-cart-item-price">{item_amount}</span>
 	<span class="edd-cart-item-separator">-</span>
-	<a href="{remove_url}" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="edd-remove-from-cart"><?php _e( 'Remove', 'easy-digital-downloads' ); ?></a>
+	<a href="{remove_url}" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="edd-remove-from-cart"><?php _e( 'Remove', 'themedd' ); ?></a>
 </li>
