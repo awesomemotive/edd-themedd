ccd5e2bc485af401e9ff7e72a7e97203061e9cb1
Andrew Munro <andrew@sumobi.com>
2018-05-03 14:48:09 +1200

    Update header viewport meta tag

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
 /**
  * The template for displaying the header
  */
-?><!DOCTYPE html>
+?><!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
