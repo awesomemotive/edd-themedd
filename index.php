6e60b844750e0aa9c153f1e2ed29deb3d7918059
Andrew Munro <andrew@sumobi.com>
2018-05-03 14:49:25 +1200

    Remove content-wrapper class from index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 get_header(); ?>
 
-<div class="content-wrapper<?php echo themedd_wrapper_classes(); ?>">
+<div class="<?php echo themedd_wrapper_classes(); ?>">
 
 	<div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
 
