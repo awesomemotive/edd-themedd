331d2a3d56b636b41d71f10577f912e1c6456f55
Andrew Munro <andrew@amdrew.com>
2018-05-09 16:37:25 +1200

    Add subtitle to 404 page + add required classes

--- 404.php
+++ 404.php
@@ -4,26 +4,29 @@
  */
 
 get_header();
-themedd_page_header( array( 'title' => __( 'Oops! That page can&rsquo;t be found.', 'themedd' ) ) );
+themedd_page_header( array( 'title' => __( 'Oops! That page can&rsquo;t be found.', 'themedd' ), 'subtitle' => __( 'It looks like nothing was found at this location. Maybe try a search?', 'themedd' ) ) );
 ?>
 
 <div class="<?php echo themedd_wrapper_classes(); ?>">
+	<?php if ( themedd_has_sidebar() ) : ?>
+	<div class="row justify-content-center">
+	<?php endif; ?>
 
-	<div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
-		<main id="main" class="site-main" role="main">
+		<div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
+			<main id="main" class="site-main" role="main">
+				<section class="error-404 not-found">
+					<div class="entry-content">
+						<?php get_search_form(); ?>
+					</div>
+				</section>
+			</main>
+		</div>
 
-            <section class="error-404 not-found">
-                <div class="page-content">
-                    <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'themedd' ); ?></p>
+		<?php themedd_get_sidebar(); ?>
 
-                    <?php get_search_form(); ?>
-                </div>
-            </section>
-
-		</main>
+	<?php if ( themedd_has_sidebar() ) : ?>	
 	</div>
-
-	<?php themedd_get_sidebar(); ?>
+	<?php endif; ?>
 
 </div>
 
