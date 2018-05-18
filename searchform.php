a1c27c647e4385a6263c1aada45af1b179f7edde
Andrew Munro <andrew@amdrew.com>
2018-05-18 14:22:10 +1200

    Class name tweaks

--- searchform.php
+++ searchform.php
@@ -7,13 +7,13 @@ $search_text = apply_filters( 'themedd_search_text', esc_attr_x( 'Search', 'plac
 ?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div class="input-group mb-3">
+	<div class="input-group input-group">
 		<label for="<?php echo $unique_id; ?>"><span class="sr-only"><?php echo _x( 'Search for:', 'label', 'themedd' ); ?></span></label>	
 		<input type="search" id="<?php echo $unique_id; ?>" class="search-field form-control" placeholder="<?php echo $search_text; ?>" value="<?php echo get_search_query(); ?>" name="s" />
 
 		<?php if ( apply_filters( 'themedd_show_search_button', true ) ) : ?>
 		<div class="input-group-append">
-			<button type="submit" class="search-submit btn btn-primary btn-lg"><span class="sr-only"><?php echo _x( 'Search', 'submit button', 'themedd' ); ?></span><?php echo Themedd_Search::search_icon(); ?></button>
+			<button type="submit" class="search-submit btn btn-search"><span class="sr-only"><?php echo _x( 'Search', 'submit button', 'themedd' ); ?></span><?php echo Themedd_Search::search_icon(); ?></button>
 		</div>
 		<?php endif; ?>
 	</div>
