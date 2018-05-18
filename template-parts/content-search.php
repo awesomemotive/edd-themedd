024e98959e3c994570c004bec0323bb79782dc27
Andrew Munro <andrew@amdrew.com>
2018-05-18 22:37:48 +1200

    Utilize themedd_page_header()
    + remove edit link

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -7,10 +7,17 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header">
-		<?php themedd_posted_on( false ); ?>
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header>
+	<?php 
+		themedd_page_header( 
+			array(
+				'posted_on'       => true,
+				'heading_size'    => 'h2',
+				'heading_classes' => array( 'entry-title' ),
+				'permalink'       => esc_url( get_permalink() ),
+				'header_classes'  => themedd_has_sidebar() ? array( 'entry-header', 'mb-3' ) : array( 'entry-header', 'py-5', 'py-lg-5' )
+			) 
+		); 
+	?>
 
 	<?php themedd_post_thumbnail(); ?>
 
@@ -18,35 +25,4 @@
 		<?php the_excerpt(); ?>
 	</div>
 
-	<?php if ( 'post' == get_post_type() ) : ?>
-
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						esc_html__( 'Edit %s', 'themedd' ),
-						the_title( '<span class="sr-only">"', '"</span>', false )
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer>
-
-	<?php else : ?>
-
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'themedd' ),
-					the_title( '<span class="sr-only">', '</span>', false )
-				),
-				'<footer class="entry-footer"><span class="edit-link">',
-				'</span></footer><!-- .entry-footer -->'
-			);
-		?>
-
-	<?php endif; ?>
 </article>
