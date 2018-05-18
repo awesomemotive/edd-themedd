80082d92f6105696e23762d5a5dc77db85b6287c
Andrew Munro <andrew@amdrew.com>
2018-05-18 22:39:00 +1200

    Search.php tweaks

--- search.php
+++ search.php
@@ -15,62 +15,70 @@ themedd_page_header( array( 'title' => $page_title, 'subtitle' => sprintf( __( '
 
 ?>
 <div class="<?php echo themedd_output_classes( themedd_wrapper_classes() ); ?>">
+	
+<?php if ( themedd_has_sidebar() ) : ?>
+	<div class="row justify-content-center">
+	<?php endif; ?>
 
-	<div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
-		<main id="main" class="site-main" role="main">
-			
-			<?php if ( have_posts() ) : ?>
-
-				<?php if ( themedd_is_edd_active() && Themedd_Search::is_product_search_results() ) : ?>
-					
-				<div class="<?php echo themedd_edd_downloads_list_wrapper_classes(); ?>">
-					
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php get_template_part( 'template-parts/download-grid' ); ?>
-
-					<?php endwhile; ?>
+		<div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
+			<main id="main" class="site-main" role="main">
 				
-					<?php
-					/**
-					* Download pagination
-					*/
-					themedd_edd_download_nav();
-					?>
-							
-				</div>
+				<?php if ( have_posts() ) : ?>
 
-				<?php else : ?>
-				
-					<?php while ( have_posts() ) : the_post();
+					<?php if ( themedd_is_edd_active() && Themedd_Search::is_product_search_results() ) : ?>
+						
+					<div class="<?php echo themedd_edd_downloads_list_wrapper_classes(); ?>">
+						
+						<?php while ( have_posts() ) : the_post(); ?>
 
+							<?php get_template_part( 'template-parts/download-grid' ); ?>
+
+						<?php endwhile; ?>
+					
+						<?php
 						/**
-						 * Run the loop for the search to output the results.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-search.php and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', 'search' );
+						* Download pagination
+						*/
+						themedd_edd_download_nav();
+						?>
+								
+					</div>
 
-					// End the loop.
-					endwhile; ?>
-				
-				<?php themedd_paging_nav(); ?>
+					<?php else : ?>
+					
+						<?php while ( have_posts() ) : the_post();
 
-				<?php endif; ?>
+							/**
+							 * Run the loop for the search to output the results.
+							 * If you want to overload this in a child theme then include a file
+							 * called content-search.php and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', 'search' );
 
-			<?php
+						// End the loop.
+						endwhile; ?>
+					
+					<?php themedd_paging_nav(); ?>
 
-			// If no content, include the "No posts found" template.
-			else :
-				get_template_part( 'template-parts/content', 'none' );
+					<?php endif; ?>
 
-			endif;
-			?>
+				<?php
 
-		</main>
+				// If no content, include the "No posts found" template.
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+				?>
+
+			</main>
+		</div>
+
+		<?php themedd_get_sidebar(); ?>
+
+	<?php if ( themedd_has_sidebar() ) : ?>	
 	</div>
-
-	<?php themedd_get_sidebar(); ?>
+	<?php endif; ?>
 
 </div>
 
