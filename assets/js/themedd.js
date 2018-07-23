890046256d520994125aec3d60d96339a0d87156
Andrew Munro <andrew@amdrew.com>
2018-07-23 20:36:48 +1200

    Recompile themedd.js

--- assets/js/themedd.js
+++ assets/js/themedd.js
@@ -4022,9 +4022,9 @@
 
 		$( '.navCart' ).removeClass('empty');
 
-		if ( typeof cartQuantityText !== 'undefined' ) {
-			var textSingular = cartQuantityText.singular,
-				textPlural   = cartQuantityText.plural,
+		if ( typeof themedd_scripts !== 'undefined' ) {
+			var textSingular = themedd_scripts.cartQuantityTextSingular,
+				textPlural   = themedd_scripts.cartQuantityTextPlural,
 				cartText     = ' ' + textPlural;
 
 			if ( response.cart_quantity === '1' ) {
@@ -4040,10 +4040,10 @@
 
 	$('body').on('edd_cart_item_removed', function( event, response ) {
 
-		if ( typeof cartQuantityText !== 'undefined' ) {
+		if ( typeof themedd_scripts !== 'undefined' ) {
 
-			var textSingular = cartQuantityText.singular,
-				textPlural   = cartQuantityText.plural,
+			var textSingular = themedd_scripts.cartQuantityTextSingular,
+				textPlural   = themedd_scripts.cartQuantityTextPlural,
 				cartText     = ' ' + textPlural;
 
 			if ( response.cart_quantity === '1' ) {
@@ -4061,5 +4061,49 @@
 		cartTotalAmount.html( response.total );
 
 	});
+
+	var navMobile = '#' + themedd_scripts.navMobile;
+	
+	$(navMobile).on('show.bs.collapse', function (e) {
+
+		var elementId = $(this).attr('id'), // ID of this collapsible menu.
+			button = $("button[data-target='#" + elementId +"']"), // Find the corresponding button that triggered the call.
+			textToChange = button.find('.navbar-toggler-text'); // Find the text to change.
+
+		if ( textToChange.length ) {
+			textToChange.text(button.data('text-menu-shown'));
+		}
+
+		toggleIcon( button, 'icon-menu-shown' );
+
+	});
+
+	$(navMobile).on('hide.bs.collapse', function (e) {
+
+		var elementId = $(this).attr('id'), // ID of this collapsible menu.
+			button = $("button[data-target='#" + elementId +"']"), // Find the corresponding button that triggered the call.
+			textToChange = button.find('.navbar-toggler-text'); // Find the text to change.
+	
+		if ( textToChange.length ) {
+			textToChange.text(button.data('text-menu-hidden'));
+		}
+
+		toggleIcon( button, 'icon-menu-hidden' );
+
+	});
+
+	// Toggle the icon.
+	function toggleIcon( button, dataAttribute ) {
+
+		var svgIcon = button.find('.icon use');
+
+		if ( svgIcon.length ) {
+			var	iconToToggle = button.data(dataAttribute);
+
+			$(svgIcon).attr('href', '#icon-' + iconToToggle);
+			$(svgIcon).get(0).setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#icon-' + iconToToggle);
+		}
+
+	}
 
 });
