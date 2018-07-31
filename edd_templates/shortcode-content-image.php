3dbc24b29279c5fd3b497636c1e5943c0ac33449
Andrew Munro <andrew@amdrew.com>
2018-07-31 21:17:48 +1200

    Pass new “size” attribute to EDD template

--- edd_templates/shortcode-content-image.php
+++ edd_templates/shortcode-content-image.php
@@ -1,7 +1,10 @@
-<?php if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail( get_the_ID() ) ) : ?>
+<?php 
+$options = themedd_edd_download_grid_options();
+
+if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail( get_the_ID() ) ) : ?>
 	<div class="edd_download_image mb-3">
 		<a href="<?php the_permalink(); ?>" class="d-block">
-			<?php echo get_the_post_thumbnail( get_the_ID(), 'thumbnail', array( 'class' => 'card-img-top' ) ); ?>
+			<?php echo get_the_post_thumbnail( get_the_ID(), $options['size'], array( 'class' => 'card-img-top' ) ); ?>
 		</a>
 	</div>
 <?php endif; ?>
