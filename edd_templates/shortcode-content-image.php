9c20dd536fe841923cbeda484a8799db374a44ea
Andrew Munro <andrew@amdrew.com>
2018-08-02 14:17:18 +1200

    Add new utility class and remove another

--- edd_templates/shortcode-content-image.php
+++ edd_templates/shortcode-content-image.php
@@ -2,9 +2,9 @@
 $options = themedd_edd_download_grid_options();
 
 if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail( get_the_ID() ) ) : ?>
-	<div class="edd_download_image mb-3">
+	<div class="edd_download_image mb-3 w-100">
 		<a href="<?php the_permalink(); ?>" class="d-block">
-			<?php echo get_the_post_thumbnail( get_the_ID(), $options['size'], array( 'class' => 'card-img-top' ) ); ?>
+			<?php echo get_the_post_thumbnail( get_the_ID(), $options['size'] ); ?>
 		</a>
 	</div>
 <?php endif; ?>
