d6356b56724e7f14ed8a5d398751f0dd15ff7a49
Andrew Munro <andrew@amdrew.com>
2018-08-03 09:49:53 +1200

    Nonce not passed when removing cart items

--- edd_templates/widget-cart-item.php
+++ edd_templates/widget-cart-item.php
@@ -3,5 +3,5 @@
 	<span class="edd-cart-item-quantity">{item_quantity} @</span>
 	<span class="edd-cart-item-price">{item_amount}</span>
 	<span class="edd-cart-item-separator">-</span>
-	<a href="{remove_url}" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="edd-remove-from-cart"><?php _e( 'Remove', 'themedd' ); ?></a>
+	<a href="{remove_url}" data-nonce="<?php echo wp_create_nonce( 'edd-remove-cart-widget-item' ); ?>" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="edd-remove-from-cart"><?php _e( 'remove', 'easy-digital-downloads' ); ?></a>
 </li>
