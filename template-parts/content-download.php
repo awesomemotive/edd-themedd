357c04dbb9d1ad9e5a2ed3f59279b97f17f185e6
Andrew Munro <andrew@amdrew.com>
2018-09-04 14:17:17 +1200

    Formatting

--- template-parts/content-download.php
+++ template-parts/content-download.php
@@ -4,20 +4,12 @@
  * Loaded by single-download.php
  */
 ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-	<?php do_action( 'themedd_entry_article_start' ); ?>
-
-    <?php themedd_post_thumbnail( array( 'size' => 'themedd-standard-image' ) ); ?>
-
-    <div class="entry-content content-wrapper">
-
-        <?php do_action( 'themedd_entry_content_start' ); ?>
-
-        <?php the_content(); ?>
-
-        <?php do_action( 'themedd_entry_content_end' ); ?>
-
-    </div>
+<?php do_action( 'themedd_entry_article_start' ); ?>
+<?php themedd_post_thumbnail( array( 'size' => 'themedd-standard-image' ) ); ?>
+	<div class="entry-content content-wrapper">
+		<?php do_action( 'themedd_entry_content_start' ); ?>
+		<?php the_content(); ?>
+		<?php do_action( 'themedd_entry_content_end' ); ?>
+	</div>
 </article>
