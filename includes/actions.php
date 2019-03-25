9f790787de2368e88892bb879912fcb9b58ef501
Andrew Munro <andrew@amdrew.com>
2019-03-25 16:27:03 +1300

    Load entry meta on themedd_header_container_end hook

--- includes/actions.php
+++ includes/actions.php
@@ -13,6 +13,18 @@ function themedd_show_entry_footer() {
 add_action( 'themedd_entry_article_end', 'themedd_show_entry_footer' );
 
 /**
+ * Show the entry meta.
+ *
+ * @since 1.1
+ */
+function themedd_show_entry_meta( $args ) {
+	if ( $args['posted_on'] ) {
+		echo themedd_posted_on( $args['show_author'] );
+	}
+}
+add_action( 'themedd_header_container_end', 'themedd_show_entry_meta', 10, 1 );
+
+/**
  * Load the biography template after the entry content on a single post.
  *
  * @since 1.0.0
